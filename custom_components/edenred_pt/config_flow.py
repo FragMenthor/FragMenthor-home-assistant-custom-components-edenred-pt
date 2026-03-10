851c223534305d71d87d55a8105d07cf92aaf1f7
FragMenthor <74264069+FragMenthor@users.noreply.github.com>
2026-03-10 11:18:27 +0000

    Refactor EdenredConfigFlow and EdenredOptionsFlow

--- custom_components/edenred_pt/config_flow.py
+++ custom_components/edenred_pt/config_flow.py
@@ -1,10 +1,8 @@
-
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 
 from .const import DOMAIN, CONF_EMAIL, CONF_PASSWORD, CONF_INTERVAL, MIN_INTERVAL, MAX_INTERVAL
-
 
 class EdenredConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -22,11 +20,10 @@ class EdenredConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required(CONF_EMAIL): str,
             vol.Required(CONF_PASSWORD): str,
             vol.Required(CONF_INTERVAL, default=60):
-                vol.All(vol.Coerce(int), vol.Range(min=MIN_INTERVAL, max=MAX_INTERVAL)),
+                vol.All(vol.Coerce(int), vol.Range(min=MIN_INTERVAL, max=MAX_INTERVAL))
         })
 
         return self.async_show_form(step_id="user", data_schema=data_schema)
-
 
 class EdenredOptionsFlow(config_entries.OptionsFlow):
     def __init__(self, config_entry):
@@ -34,11 +31,16 @@ class EdenredOptionsFlow(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Opções", data=user_input)
+            return self.async_create_entry(title="Opções", data={CONF_INTERVAL: user_input[CONF_INTERVAL]})
+
+        current = self.config_entry.options.get(
+            CONF_INTERVAL,
+            self.config_entry.data.get(CONF_INTERVAL, 60)
+        )
 
         data_schema = vol.Schema({
-            vol.Required(CONF_INTERVAL, default=self.config_entry.data.get(CONF_INTERVAL, 60)):
-                vol.All(vol.Coerce(int), vol.Range(min=MIN_INTERVAL, max=MAX_INTERVAL)),
+            vol.Required(CONF_INTERVAL, default=current):
+                vol.All(vol.Coerce(int), vol.Range(min=MIN_INTERVAL, max=MAX_INTERVAL))
         })
 
         return self.async_show_form(step_id="init", data_schema=data_schema)
